e56a8e4cb4e4013a061aa51458db89fcc610f63c
Milan Höllner <milan.hoellner@gmail.com>
2017-09-29 18:01:28 +0200

    add VS project file add .gitignore some thoughts on refactoring

--- examples.cs
+++ examples.cs
@@ -1,26 +1,50 @@
-namespace StableMatchingAlgorithm {
-  static public class Examples {
-    static public int[,] loadPrefsSize5(int option) {
-      const int size = 5;
-      int[,] prefs = null;
-
-      if (option == 1){
-	 prefs = new int[size, size]{
-	 {1, 4, 0, 2, 3},
-	 {0, 1, 2, 3, 4},
-	 {1, 2, 4, 3, 0},
-	 {0, 2, 1, 3, 4},
-	 {4, 2, 1, 0, 3}};
-      }
-      else if(option == 2){
-	 prefs = new int[size,size]{
-	 {4, 0, 3, 1, 2},
-	 {3, 4, 1, 0, 2},
-	 {0, 3, 1, 2, 4},
-	 {2, 1, 3, 0, 4},
-	 {3, 1, 2, 4, 0}};
-      }
-      return prefs;
+namespace StableMatcher
+{
+    public static class Examples
+    {
+        public static readonly int[,] Moodle3Men = new int[3, 3]
+        {
+            { 1, 0, 2 },
+            { 2, 1, 0 },
+            { 0, 2, 1 }
+        };
+        public static readonly int[,] Moodle3Women = new int[3, 3]
+        {
+            { 1, 0, 2 },
+            { 2, 1, 0 },
+            { 0, 2, 1 }
+        };
+        public static readonly int[,] Moodle5Men = new int[5, 5]
+        {
+            { 0, 1, 4, 3, 2 },
+            { 1, 4, 3, 0, 2 },
+            { 4, 1, 0, 2, 3 },
+            { 3, 0, 1, 2, 4 },
+            { 4, 3, 1, 2, 0 }
+        };
+        public static readonly int[,] Moodle5Women = new int[5, 5]
+        {
+            { 2, 1, 3, 0, 4 },
+            { 3, 4, 0, 1, 2 },
+            { 1, 2, 3, 4, 0 },
+            { 0, 1, 2, 4, 3 },
+            { 0, 1, 4, 2, 3 }
+        };
+        public static readonly int[,] SampleMen = new int[5,5]
+        {
+            { 1, 4, 0, 2, 3 },
+            { 0, 1, 2, 3, 4 },
+            { 1, 2, 4, 3, 0 },
+            { 0, 2, 1, 3, 4 },
+            { 4, 2, 1, 0, 3 }
+        };
+        public static readonly int[,] SampleWomen = new int[5,5]
+        {
+            { 4, 0, 3, 1, 2 },
+            { 3, 4, 1, 0, 2 },
+            { 0, 3, 1, 2, 4 },
+            { 2, 1, 3, 0, 4 },
+            { 3, 1, 2, 4, 0 }
+        };
     }
-  }
 }
--- stableMatcher.cs
+++ stableMatcher.cs
@@ -1,38 +1,43 @@
 using System;
 
-namespace StableMatchingAlgorithm {
-  public class StableMatcher
-  {
-      static public void Main ()
-      {
-	  Console.WriteLine ("Gale-Shapley Stable Matching algorithm");
-	  int[,] menPrefs = Examples.loadPrefsSize5(1);
-	  int[,] womenPrefs = Examples.loadPrefsSize5(2);
+namespace StableMatcher
+{
+    public class StableMatcher
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Gale-Shapley Stable Matching algorithm");
+            int[,] menPrefs = Examples.Moodle5Men; //SampleMen;
+            int[,] womenPrefs = Examples.Moodle5Women; //SampleWomen;
 
-	  Console.WriteLine ("Show preferences for men");
-	  showPrefs(menPrefs);
-	  Console.WriteLine ("Show preferences for women");
-	  showPrefs(womenPrefs);
+            Console.WriteLine("Show preferences for men");
+            ShowPrefs(menPrefs);
+            Console.WriteLine("Show preferences for women");
+            ShowPrefs(womenPrefs);
 
-	  int[] fiancee = Utils.findStableMatchesUsingGaleShapleyAlgorithm(menPrefs, womenPrefs);
-	  //show results            
-	  Console.WriteLine("Solution to stable matiching problem:");
-	  for (int i = 0; i < fiancee.Length; i++){
-	      Console.Write("W{0}-M{1}, ", i, fiancee[i]);
-	  }
-	  Console.WriteLine();
-      }
+            int[] fiancee = Utils.FindStableMatchesUsingGaleShapleyAlgorithm(menPrefs, womenPrefs);
+            //show results            
+            Console.WriteLine("Solution to stable matching problem:");
+            for (int i = 0; i < fiancee.Length; i++)
+            {
+                Console.Write("W{0}-M{1}, ", i + 1, fiancee[i] + 1);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
 
-      static public void showPrefs(int[,] prefs)
-      {
-	  int size = prefs.GetLength(0);
-	  for(int row = 0; row < size; row++){
-	      Console.Write("{0}: ", (row).ToString());
-	      for(int col = 0; col < size; col++){
-		  Console.Write("{0} ", prefs[row,col].ToString());
-	      }
-	      Console.WriteLine("");
-	  }
-      }
-  }
+        public static void ShowPrefs(int[,] prefs)
+        {
+            int size = prefs.GetLength(0);
+            for (int row = 0; row < size; row++)
+            {
+                Console.Write("{0}: ", row + 1);
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write("{0} ", prefs[row, col] + 1);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
 }
